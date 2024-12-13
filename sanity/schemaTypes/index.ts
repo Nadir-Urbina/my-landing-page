3cdd90f26192b643a2d217499ff24b9ea6b59bb3
Nadir Urbina <nurbinabr@gmail.com>
2024-12-13 14:01:40 -0500

    removing healing streams from sanity

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -3,6 +3,5 @@ import testimonial from '../schemas/testimonial'
 import event from '../schemas/event'
 import book from '../schemas/book'
 import mission from '../schemas/mission'
-import healingStreams from '../schemas/healingStreams'
 
-export const schemaTypes = [testimonial, event, book, mission, healingStreams]
+export const schemaTypes = [testimonial, event, book, mission]
