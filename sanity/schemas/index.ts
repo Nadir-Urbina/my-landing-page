c8248db0bcd2f0369c3596d0268f1ca826a8d441
Nadir Urbina <nurbinabr@gmail.com>
2024-12-13 13:38:20 -0500

    fixing issues with healing streams sub page

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -2,12 +2,10 @@ import book from './book'
 import testimonial from './testimonial'
 import event from './event'
 import mission from './mission'
-import healingStreams from './healingStreams'
 
 export const schemaTypes = [
   book,
   testimonial,
   event,
   mission,
-  healingStreams,
 ] 
