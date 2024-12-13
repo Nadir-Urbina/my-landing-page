4513cff7b27833656ed85e0d4d35bfc4a15e08a1
Nadir Urbina <nurbinabr@gmail.com>
2024-12-13 13:41:22 -0500

    ts sanity error with healing streams

--- sanity/schemaTypes.ts
+++ sanity/schemaTypes.ts
@@ -2,12 +2,10 @@ import testimonial from './schemas/testimonial'
 import mission from './schemas/mission'
 import event from './schemas/event'
 import book from './schemas/book'
-import healingStreams from './schemas/healingStreams'
 
 export const schemaTypes = [
   testimonial,
   mission,
   event,
   book,
-  healingStreams,
 ] 
